1dfdf89f1ce43007e39d9c725c9be61498c9f659
eddiemock <87999582+eddiemock@users.noreply.github.com>
2023-03-14 15:12:51 +0000

    accessibility changes

--- app/Views/Home2.php
+++ app/Views/Home2.php
@@ -1,3 +1,4 @@
+<?= view('templates/accessibility');?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,14 +10,14 @@
    <title>Everybody Welcome</title>
    <link href="<?= base_url(); ?>/assets/css/style_theme.css" rel="stylesheet">
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
+   <link rel="stylesheet" href="./assets/css/accessiblity.css" />
    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
    <link href="<?= base_url(); ?>/assets/font-awesome/css/font-awesome.css" rel="stylesheet">
    <!-- <meta http-equiv="refresh" content="5"> -->
 
    <link href="<?= base_url(); ?>/assets/css/venueSearch/style.css" rel="stylesheet">
 
-
+  
    <style>
       /* chips */
       .chip-container {

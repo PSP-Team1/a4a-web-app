61425f49be9e1399b3cb92a67899c19cb4c485da
JacobSmith333 <thejacob976@gmail.com>
2023-03-27 16:14:28 +0100

    Updated FAQ questions

--- app/Views/FAQ.php
+++ app/Views/FAQ.php
@@ -25,6 +25,7 @@
 	<div class="container">
 		
     <h1 class="h1">Frequently Asked Questions</h1>
+	<h2>Standard Questions </h2>
   
 		<div class="card">
 			<div class="card-header">
@@ -40,8 +41,40 @@
 					This ensures that whatever your disability, it will be taken into account to allow you to know whether a business is suitable for you.
 				</div>
 			</div>
+
+			<div class="card">
+			<div class="card-header">
+				<h3 class="mb-0">
+					<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse6" aria-expanded="true" aria-controls="collapse1">
+						Question: How can I find businesses to visit?
+					</button>
+				</h3>
+			</div>
+			<div id="collapse6" class="collapse" aria-labelledby="heading1" data-parent="#accordion">
+				<div class="card-body">
+					There a multiple ways to find businesses on our search page, as you can search by location or type to find somewhere that fits your needs
+				</div>
+			</div>
 		</div>
-		
+		<div class="card">
+			<div class="card-header">
+				<h3 class="mb-0">
+					<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse7" aria-expanded="true" aria-controls="collapse1">
+						Question: Is there any way to leave feedback?
+					</button>
+				</h3>
+			</div>
+			<div id="collapse7" class="collapse" aria-labelledby="heading1" data-parent="#accordion">
+				<div class="card-body">
+					We have a review system that allows you to give your thoughts on a business you have visited which can then be used by other people
+					considering visiting to further inform their visit along with the data provided through the audit
+				</div>
+			</div>
+		</div>
+
+		</div>
+		<br>
+		<h2>Business Questions </h2>
     <div class="card">
 		<div class="card-header">
 			<h3 class="mb-0">
@@ -74,8 +107,26 @@
 		</div>
 	</div>
 
+	<div class="card">
+			<div class="card-header">
+				<h3 class="mb-0">
+					<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse8" aria-expanded="true" aria-controls="collapse1">
+						Question: How well advertised will my business be?
+					</button>
+				</h3>
+			</div>
+			<div id="collapse8" class="collapse" aria-labelledby="heading1" data-parent="#accordion">
+				<div class="card-body">
+					We have a complex algorithm that determines search preference, which includes factors such as accessibility score, customer reviews
+					and premium customer status, all of which mix together to ensure that more compelete businesses will be featured further up in the search rankings
+				</div>
+			</div>
+		</div>
+
 	<br>
 	<a href="<?= base_url() ?>/home" class="btn btn-light">Return To Homepage</a>
+	<br>
+	<br>
 	
 </div>
 

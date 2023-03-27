a9c51692780ffb76e61d89327929cb2f715366cc
krlwshu <93937576+krlwshu@users.noreply.github.com>
2023-03-27 19:41:04 +0100

    Updated Stripe rev dash

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -73,6 +73,7 @@ $routes->group('', ['filter' => 'authGuard'], function ($routes) {
     $routes->get('/Audit', 'AuditController::index');
     // $routes->get('/Audit/auditConfirmation', 'AuditController::auditConfirmation');
     $routes->get('success', 'PaymentController::success');
+    $routes->get('showTransactions', 'PaymentController::showTransactions');
     $routes->get('/Audit/auditConfirmation/(:num)', 'AuditController::auditConfirmation/$1');
     $routes->get('/AuditController/OpenAudit/(:num)', 'AuditController::openAudit/$1');
     $routes->post('/Audit/completeAudit', 'AuditController::completeAudit');
--- app/Controllers/PaymentController.php
+++ app/Controllers/PaymentController.php
@@ -52,4 +52,67 @@ class PaymentController extends BaseController
     {
         return view('payment/success');
     }
+
+
+    public function showTransactions()
+    {
+        // Set your Stripe API key
+        Stripe::setApiKey('sk_test_51MqHD3BuIYAzoRP9AlmWRsf18gGMHDSl3qA60rmanjm4klpjUfbbX8DLZJFzMI6GkxpFhjmpadqCBhUKfSmP4Lu800vJ7fVGba');
+
+        // Retrieve the list of charges
+        $charges = Charge::all([
+            'limit' => 10,
+        ]);
+
+        // Get the transaction data for the chart
+        $chartData = $this->getTransactionChartData();
+
+        // Pass the charges and chart data to the view
+        return view('payment/transactions', [
+            'charges' => $charges,
+            'chartData' => $chartData,
+        ]);
+    }
+
+    private function getTransactionChartData()
+    {
+
+        //gget 24 hr rev stats for chart
+        $endDate = time();
+        $startDate = strtotime('-24 hours', $endDate);
+
+
+        //could do with setting this globally somewhere.
+        Stripe::setApiKey('sk_test_51MqHD3BuIYAzoRP9AlmWRsf18gGMHDSl3qA60rmanjm4klpjUfbbX8DLZJFzMI6GkxpFhjmpadqCBhUKfSmP4Lu800vJ7fVGba');
+
+        $charges = Charge::all([
+            'created' => [
+                'gte' => $startDate,
+                'lte' => $endDate,
+            ],
+            'limit' => 100,
+        ]);
+
+        $chartData = [
+            'labels' => [],
+            'data' => [],
+        ];
+
+        foreach ($charges->data as $charge) {
+            $date = date('Y-m-d H:00', $charge->created);
+            if (isset($chartData['data'][$date])) {
+                $chartData['data'][$date]++;
+            } else {
+                $chartData['data'][$date] = 1;
+                $chartData['labels'][] = $date;
+            }
+        }
+
+        ksort($chartData['data']);
+
+        // Convert the chart data to the format expected by Chart.js
+        $chartData['data'] = array_values($chartData['data']);
+
+        return $chartData;
+    }
 }
